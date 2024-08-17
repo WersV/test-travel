befad7905f7e871b31cfea83796c5e8e6d21ab01
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-08-17 14:25:04 +0200

    set dates

--- src/test/java/pl/travel/HotelSearch.java
+++ src/test/java/pl/travel/HotelSearch.java
@@ -14,24 +14,33 @@ import org.testng.annotations.Test;
 public class HotelSearch {
 
     @Test
-    public void searchHotel() {
+    public void searchHotel() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-search-engine-choice-screen");
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
+        //type city name
         driver.get("http://www.kurs-selenium.pl/demo/");
-//        WebElement searchInput = driver.findElement(By.className("select2-focusser")); //alternative way of searching for this input
         WebElement searchInput = driver.findElement(By.xpath("//input[contains(@class, 'select2-focusser')]"));
         searchInput.sendKeys("Dubai");
-
         String confirmElement = "//span[@class='select2-match' and text()='Dubai']";
-
         WebDriverWait wait = new WebDriverWait(driver, 2);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(confirmElement)));
-
         driver.findElement(By.xpath(confirmElement)).click();
 
+        //set dates by writing dates
+        WebElement checkInInput = driver.findElement(By.name("checkin"));
+//        checkInInput.sendKeys("22/08/2024");
+        WebElement checkOutInput = driver.findElement((By.name("checkout")));
+//        checkOutInput.sendKeys("30/08/2024");
+
+        //set dates by using datepicker
+        checkInInput.click();
+        driver.findElement(By.xpath("//div[@class='datepicker-days']//td[text()='22']")).click();
+//        driver.findElements(By.xpath("//div[@class='datepicker-days']//td[text()='30']")).stream().filter(el -> el.isDisplayed()).skip(1).findFirst().ifPresent(el -> el.click());
+        driver.findElements(By.xpath("//div[@class='datepicker-days']//td[not(contains(@class, 'disabled')) and text()='30']")).stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click()); //without skip() method
     }
 }
