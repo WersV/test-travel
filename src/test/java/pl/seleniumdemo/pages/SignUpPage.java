ec0bff2eaa85e8d5a46bed74f34e22c21302571e
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-08-19 19:10:30 +0200

    implement page object pattern in SignUpTest

---
+++ src/test/java/pl/seleniumdemo/pages/SignUpPage.java
@@ -0,0 +1,77 @@
+package pl.seleniumdemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class SignUpPage {
+
+    @FindBy(xpath = "//li[@id='li_myaccount']//a[contains(text(),' My Account ')]")
+    private List<WebElement> myAccMenu;
+
+    @FindBy(xpath = "//ul[@class='dropdown-menu']//a[text()='  Sign Up']")
+    private List<WebElement> signUpOption;
+
+    @FindBy(xpath = "//input[@name='firstname']")
+    private WebElement firstName;
+
+    @FindBy(xpath = "//input[@name='lastname']")
+    private WebElement lastName;
+
+    @FindBy(xpath = "//input[@name='phone']")
+    private WebElement phoneNum;
+
+    @FindBy(xpath = "//input[@name='email']")
+    private WebElement email;
+
+    @FindBy(xpath = "//input[@name='password']")
+    private WebElement password;
+
+    @FindBy(xpath = "//input[@name='confirmpassword']")
+    private WebElement confirmpassword;
+
+    @FindBy(xpath = "//button[text()=' Sign Up']")
+    private WebElement signUpBtn;
+
+    @FindBy(xpath = "//h3[contains(text(), 'Hi, ')]")
+    private WebElement userName;
+
+    private final WebDriver driver;
+
+    public SignUpPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void enterSignUpPage() {
+        myAccMenu.stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
+        signUpOption.stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
+    }
+
+    public void fillSignUpForm(String firstName, String lastName, String phoneNum, String email, String password, String confirmPassword) {
+        this.firstName.sendKeys(firstName);
+        this.lastName.sendKeys(lastName);
+        this.phoneNum.sendKeys(phoneNum);
+        this.email.sendKeys(email);
+        this.password.sendKeys(password);
+        this.confirmpassword.sendKeys(confirmPassword);
+    }
+
+    public void performSignUp() {
+        signUpBtn.click();
+    }
+
+    public void checkNameAfterSignUp() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[contains(text(), 'Hi, ')]")));
+        Assert.assertTrue(userName.getText().contains(this.firstName.getAttribute("value")));
+        Assert.assertTrue(userName.getText().contains(this.lastName.getAttribute("value")));
+    }
+}
--- src/test/java/pl/seleniumdemo/tests/SignUpTest.java
+++ src/test/java/pl/seleniumdemo/tests/SignUpTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import pl.seleniumdemo.pages.SignUpPage;
 
 public class SignUpTest extends BaseTest {
 
@@ -16,31 +17,10 @@ public class SignUpTest extends BaseTest {
 
     @Test
     public void signUp() {
-        //sign up
-        driver.findElements(By.xpath("//li[@id='li_myaccount']//a[contains(text(),' My Account ')]")).stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
-        driver.findElements(By.xpath("//ul[@class='dropdown-menu']//a[text()='  Sign Up']")).stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
-
-        String firstName = "John";
-        String lastName = "Doe";
-        String emailRandomPart = randomEmailSeed();
-
-        //fill sign up form
-        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys(firstName);
-        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys(lastName);
-        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("123456789");
-        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(emailRandomPart + "@gmail.com");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("secretpass1");
-        driver.findElement(By.xpath("//input[@name='confirmpassword']")).sendKeys("secretpass1");
-        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
-
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h3[contains(text(), 'Hi, ')]")));
-
-        //check name after sign up
-        String userNameCheck = driver.findElement(By.xpath("//h3[contains(text(), 'Hi, ')]")).getText();
-        System.out.println(userNameCheck);
-
-        Assert.assertTrue(userNameCheck.contains(firstName));
-        Assert.assertTrue(userNameCheck.contains(lastName));
+        SignUpPage signUpPage = new SignUpPage(driver);
+        signUpPage.enterSignUpPage();
+        signUpPage.fillSignUpForm("John", "Doe", "123456789", randomEmailSeed()+"@gmail.com", "admin1", "admin1");
+        signUpPage.performSignUp();
+        signUpPage.checkNameAfterSignUp();
     }
 }
