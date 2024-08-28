ccf34fac8b7e1f6e599207b9bdf224f9d6d0511e
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-08-28 22:13:09 +0200

    add DriverFactory class

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -6,17 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pl.seleniumdemo.utils.DriverFactory;
 
-public class BaseTest {
+public class BaseTest extends DriverFactory {
 
     protected WebDriver driver;
 
     @BeforeMethod
     public void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-search-engine-choice-screen");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
+        driver = getDriver("firefox");
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
     }
---
+++ src/test/java/pl/seleniumdemo/utils/DriverFactory.java
@@ -0,0 +1,28 @@
+package pl.seleniumdemo.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Objects;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver(String driverType) {
+
+        WebDriver driver = null;
+
+        if (Objects.equals(driverType, "chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-search-engine-choice-screen");
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+        } else if (Objects.equals(driverType, "firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+        return driver;
+    }
+}
