b4d4a0fc042e1f30c5fa8002c958b1600fefb562
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-08-29 21:59:22 +0200

    add quiting driver after performing each teast

--- src/test/java/pl/seleniumdemo/tests/BaseTest.java
+++ src/test/java/pl/seleniumdemo/tests/BaseTest.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import pl.seleniumdemo.utils.DriverFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest extends DriverFactory {
 
@@ -14,14 +18,15 @@ public class BaseTest extends DriverFactory {
 
     @BeforeMethod
     public void setup() {
-        driver = getDriver("firefox");
+        driver = getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("http://www.kurs-selenium.pl/demo/");
     }
 
-//    @AfterMethod
-//    public void tearDown() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
